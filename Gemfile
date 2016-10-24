fe677719ab2da1c7d13661db106969067e87838b
Lily Lee <l.lee@yottaa.com>
2016-10-24 19:12:50 -0400

    Make a basic User model (including secure passwords)

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ source 'https://rubygems.org'
 gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 
 gem 'bootstrap-sass', '3.3.6'
+
+gem 'bcrypt',         '3.1.11'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 # Use Puma as the app server
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ Rails.application.routes.draw do
   #get  '/home', to: 'static_pages#home'
 
   get  '/signup',  to: 'users#new'
+  resources :users
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
