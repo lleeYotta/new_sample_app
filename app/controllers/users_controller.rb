03959167217aaa994e9a3583fa8ce49c7645b714
Lily Lee <l.lee@yottaa.com>
2016-10-26 14:51:59 -0400

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,33 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
   end
+
+   def show
+    @user = User.find(params[:id])
+     #debugger
+  end
+
+  def create
+
+  	 #@user = User.new(params[:user])    # Not the final implementation!
+
+  	 @user = User.new(user_params)
+    if @user.save
+      # Handle a successful save.
+      #redirect_to @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to user_url(@user)
+    else
+      render 'new'
+    end
+  	
+  end
+
+  def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   #get  '/home', to: 'static_pages#home'
 
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
   resources :users
 
 
